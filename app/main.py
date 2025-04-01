614b86f19ab4ce037834b0e6446a728051950fd7
slipstr34m <shresth@grapevine.in>
2025-04-01 09:07:39 +0000

    add: private ip only

--- app/main.py
+++ app/main.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from starlette.responses import JSONResponse
 from .routes import user_profile
+from ipaddress import ip_address
+
 
 app = FastAPI()
 
+class BlockPublicIPMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        async def asgi(receive, send):
+            client_ip = scope["client"][0]
+            try:
+                ip = ip_address(client_ip)
+                if ip.is_global:
+                    raise HTTPException(status_code=403, detail="Public IP access forbidden")
+            except ValueError:
+                pass  # Invalid IP address, continue processing
+            return await self.app(scope, receive, send)
+        
+        try:
+            await asgi(receive, send)
+        except HTTPException as exc:
+            response = JSONResponse(content={"detail": exc.detail}, status_code=exc.status_code)
+            await response(scope, receive, send)
+
+
 # Include routes
 app.include_router(user_profile.router)
-
+app.add_middleware(BlockPublicIPMiddleware)
